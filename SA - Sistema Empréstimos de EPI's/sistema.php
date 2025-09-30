210f79ff4fcfe888b090eb49df817bdc6c242586
P-lentz <gui.mplentz@gmail.com>
2025-09-30 20:16:16 -0300

    LOGIN + REDIRECIONA SISTEMA

--- SA - Sistema Empréstimos de EPI's/sistema.php
+++ SA - Sistema Empréstimos de EPI's/sistema.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sistema - EPIg Seguros</title>
     
-    <link href="assets/css/index.css" rel="stylesheet">
+    <link href="assets/css/sistema.css" rel="stylesheet">
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     
