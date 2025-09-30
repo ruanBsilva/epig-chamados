52ba559106774b4d29300d9be8cdcf2e8ef8ada6
P-lentz <gui.mplentz@gmail.com>
2025-09-30 19:39:57 -0300

    tesste

--- SA - Sistema Empréstimos de EPI's/index.php
+++ SA - Sistema Empréstimos de EPI's/index.php
@@ -26,10 +26,10 @@
     <main class="form-signin w-100 m-auto">
         <form onsubmit="return false">
             <img class="mb-4" src="assets/img/EPIG_LOGO.png">
-            <h1 class="h3 mb-3 fw-normal text-center">Acesso ao Sistema</h1>
+            <h1 class="h3 mb-3 fw-normal text-center">Acesso ao Sistemaaa</h1>
             <div class="form-floating">
                 <input type="text" class="form-control" id="txt-usuario">
-                <label for="txt-usuario">Usuário</label>
+                <label for="txt-usuario">Usuáriiio</label>
             </div>
             <div class="form-floating input-group mb-3">
                 <input type="password" class="form-control" id="txt-senha">
