66efbd6f8e17f949190f064f0c74058f294d27f4
ruanBsilva <ruan_b_silva@estudante.sc.senai.br>
2025-09-30 21:52:49 -0300

    sistemaAtulizado2.0

--- SA - Sistema Empréstimos de EPI's/sistema.php
+++ SA - Sistema Empréstimos de EPI's/sistema.php
@@ -11,6 +11,7 @@ session_start();
     <title>Sistema - EPIg Seguros</title>
     
     <link href="assets/css/sistema.css" rel="stylesheet">  
+    <link href="assets/css/usuarios.css" rel="stylesheet">  
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
 </head>
