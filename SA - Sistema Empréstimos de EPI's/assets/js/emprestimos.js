887385636f0476332e596cd8201ac3b14a0fd2dc
P-lentz <gui.mplentz@gmail.com>
2025-11-24 20:19:24 -0300

    funfando

--- SA - Sistema Empréstimos de EPI's/assets/js/emprestimos.js
+++ SA - Sistema Empréstimos de EPI's/assets/js/emprestimos.js
@@ -1,49 +1,47 @@
 function inicializarEmprestimos() {
-    listarColaborador()
-    listarEquipamento()
+    listColaborador();
+    listEquipamento();
 }
 
-function listarColaborador(){
+function listColaborador() {
     $.ajax({
         type: 'post',
         dataType: 'json',
         url: 'src/colaborador/selectTodos.php',
-        success: function(resposta){
+        success: function (resposta) {
             var listarColaboradores = document.getElementById('list-colaborador')
             listarColaboradores.innerHTML = '';
 
             var colaboradores = resposta['dados'];
             var opcao = "<option value=''>Selecione o colaborador...</option>";
-            colaboradores.forEach(function(colaborador){
+            colaboradores.forEach(function (colaborador) {
                 opcao += `<option value='${colaborador['idColaborador']}'>${colaborador['nome']}</option>`;
             });
             listarColaboradores.innerHTML = opcao;
-
-            console.log("aqui")
         },
-        error: function(erro){
+        error: function (erro) {
             alert("Ocorreu um erro: " + erro);
         }
     });
 }
 
-function listarEquipamento(){
+function listEquipamento() {
     $.ajax({
         type: 'post',
         dataType: 'json',
         url: 'src/equipamento/selecionarTodos.php',
-        success: function(resposta){
+        success: function (resposta) {
             var listarEquipamentos = document.getElementById('list-equipamento')
             listarEquipamentos.innerHTML = '';
 
             var equipamentos = resposta.dados;
             var opcao = "<option value=''>Selecione o equipamento...</option>";
-            equipamentos.forEach(function(equipamento){
+            equipamentos.forEach(function (equipamento) {
                 opcao += `<option value='${equipamento['id_equipamento']}'>${equipamento['nome_equip']}</option>`;
             });
             listarEquipamentos.innerHTML = opcao;
         },
-        error: function(erro){
+        error: function (erro) {
             alert("Ocorreu um erro: " + erro);
         }
     });
